691f85015ed4b76c85a14f1779459bef4a0b570b
Neharika Padala <npadala@gdresearchcenter.com>
2019-05-09 17:01:17 +0530

    checking

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The base configuration for WordPress
- * Neharika Padala
+ * Neharika Padala check 1
  * The wp-config.php creation script uses this file during the
  * installation. You don't have to use the web site, you can
  * copy this file to "wp-config.php" and fill in the values.
